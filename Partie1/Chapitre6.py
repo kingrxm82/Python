1709bf8898076188fa71a956339d6a2bcb3a67a9
Ramzi Mejri <kingrxm82@gmail.com>
2024-01-29 12:34:03 +0100

    Chapitre 6

---
+++ Partie1/Chapitre6.py
@@ -0,0 +1,17 @@
+"Importez des packages Python"
+
+#Importez un module
+import package.modules
+resultat = package.modules.sum(10,20)
+print(resultat)
+from package.modules import sum
+resultat=sum(10,20)
+print(resultat)
+
+#Installer les packages avec pip
+"pip install <nom-du-package>"
+"pip freeze"
+
+#Réferences
+"Chapitre 5"
+"https://openclassrooms.com/fr/courses/7168871-apprenez-les-bases-du-langage-python/7296681-importez-des-packages-python"
---
+++ Partie1/package/modules.py
@@ -0,0 +1,13 @@
+"Creation de package et leurs modules"
+def sum (a,b):
+    return a+b
+def subs (a,b):
+    return a-b
+def mult(a,b):
+    return a*b
+def div(a,b):
+    a/b
+    
+#Réferences
+"Chapitre 5"
+"https://openclassrooms.com/fr/courses/7168871-apprenez-les-bases-du-langage-python/7296681-importez-des-packages-python"
