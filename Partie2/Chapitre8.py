aaa38d9e99260ff8726ba180ef77cdd9571fa474
Ramzi Mejri <kingrxm82@gmail.com>
2024-02-02 11:19:48 +0100

    Chapitre 8

---
+++ Partie2/Chapitre8.py
@@ -0,0 +1,35 @@
+class User:
+    def __init__(self, username, password):
+        "Initialise le nom et le mot de passe."
+        if len(username) < 2:
+            raise UsernameTooShortException()
+        if len(password) < 10:
+            raise PasswordTooShortException()
+
+        self.username = username
+        self.password = password
+
+
+class UsernameTooShortException(Exception):
+    def __init__(self, msg="", *args, **kwargs):
+        msg = msg or "Le nom d'utilisateur est trop court !"
+        super().__init__(msg, *args, **kwargs)
+
+
+class PasswordTooShortException(Exception):
+    def __init__(self, msg="", *args, **kwargs):
+        msg = msg or "Le mot de passe est trop court !"
+        super().__init__(msg, *args, **kwargs)
+
+
+if __name__ == "__main__":
+    user = User("John", "supersecret")
+    try:
+        user = User("t", "supersecret")
+    except UsernameTooShortException:
+        print("L'exception sur le nom d'utilisateur a été levée.")
+
+    try:
+        user = User("John", "t")
+    except PasswordTooShortException:
+        print("L'exception sur le mot de passe a été levée.")
